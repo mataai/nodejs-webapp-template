f9a10020c117976877df4f3ce34fedf6a3d2e81d
Matai <code@matai.info>
2025-11-11 19:53:05 -0500

    enable and config lint

--- apps/nodejs-webapp-template/src/app/app.spec.ts
+++ apps/nodejs-webapp-template/src/app/app.spec.ts
@@ -1,4 +1,5 @@
 import { TestBed } from '@angular/core/testing';
+
 import { App } from './app';
 import { NxWelcome } from './nx-welcome';
 
@@ -14,7 +15,7 @@ describe('App', () => {
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('h1')?.textContent).toContain(
-      'Welcome nodejs-webapp-template'
+      'Welcome nodejs-webapp-template',
     );
   });
 });
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,21 @@
 import nx from '@nx/eslint-plugin';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
 
 export default [
   ...nx.configs['flat/base'],
   ...nx.configs['flat/typescript'],
   ...nx.configs['flat/javascript'],
+  ...nx.configs['flat/angular'],
+  ...nx.configs['flat/angular-template'],
   {
-    ignores: ['**/dist'],
+    ignores: [
+      '**/dist',
+      '**/vite.config.*.timestamp*',
+      '**/vitest.config.*.timestamp*',
+    ],
+    plugins: {
+      'simple-import-sort': simpleImportSort,
+    },
   },
   {
     files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
@@ -23,20 +33,93 @@ export default [
           ],
         },
       ],
+      'comma-dangle': [
+        'warn',
+        {
+          arrays: 'always-multiline',
+          imports: 'always-multiline',
+          exports: 'always-multiline',
+          objects: 'always-multiline',
+          functions: 'always-multiline',
+        },
+      ],
+      'simple-import-sort/exports': 'error',
+      'no-duplicate-imports': 'error',
+      '@typescript-eslint/no-explicit-any': 'error',
+      '@typescript-eslint/no-unused-vars': 'error',
+      '@typescript-eslint/explicit-function-return-type': 'error',
+      '@typescript-eslint/explicit-member-accessibility': [
+        'error',
+        {
+          overrides: {
+            constructors: 'off',
+          },
+        },
+      ],
+      'simple-import-sort/imports': [
+        'error',
+        {
+          groups: [
+            [
+              '^(@angular)(.*|$)',
+              '^(@nestjs)(.*|$)',
+              '^(@typeorm)(.*|$)',
+              '^(rxjs)(.*|$)',
+            ],
+            ['^(?!(@angular|rxjs|@next.lanets.ca|@lanets|./|../)).*$'],
+            ['^(@next.lanets.ca|@lanets)(.*|$)'],
+            ['^(../)(.*|$)', '^(./)(.*|$)'],
+          ],
+        },
+      ],
+      'no-alert': 'error',
     },
   },
   {
-    files: [
-      '**/*.ts',
-      '**/*.tsx',
-      '**/*.cts',
-      '**/*.mts',
-      '**/*.js',
-      '**/*.jsx',
-      '**/*.cjs',
-      '**/*.mjs',
-    ],
+    files: ['**/*.ts'],
+    rules: {
+      '@angular-eslint/directive-selector': [
+        'error',
+        {
+          type: 'attribute',
+          prefix: 'app',
+          style: 'camelCase',
+        },
+      ],
+      '@angular-eslint/component-selector': [
+        'error',
+        {
+          type: 'element',
+          prefix: 'app',
+          style: 'kebab-case',
+        },
+      ],
+    },
+  },
+  {
+    files: ['**/*.html'],
     // Override or add rules here
-    rules: {},
+    rules: {
+      '@typescript-eslint/adjacent-overload-signatures': 'off',
+      '@angular-eslint/template/eqeqeq': 'error',
+      '@angular-eslint/template/alt-text': 'error',
+      '@angular-eslint/template/elements-content': 'error',
+      '@angular-eslint/template/table-scope': 'error',
+      '@angular-eslint/template/banana-in-box': 'error',
+      '@angular-eslint/template/use-track-by-function': 'error',
+      '@angular-eslint/template/button-has-type': 'error',
+      '@angular-eslint/template/no-positive-tabindex': 'error',
+      '@angular-eslint/template/click-events-have-key-events': 'error',
+      '@angular-eslint/template/mouse-events-have-key-events': 'error',
+      '@angular-eslint/template/conditional-complexity': [
+        'error',
+        {
+          maxComplexity: 3,
+        },
+      ],
+      '@angular-eslint/template/no-any': 'error',
+      '@angular-eslint/template/no-autofocus': 'error',
+      '@angular-eslint/template/no-duplicate-attributes': ['error'],
+    },
   },
 ];
