0ad80a50336976a229ca6805274618e056ebbccd
Matai <code@matai.info>
2025-11-27 18:02:49 -0500

    create expo mobile app

---
+++ apps/mobile/jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  displayName: 'mobile',
+  preset: '../../jest.preset.js',
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+  transform: {
+    '^.+\\.[tj]sx?$': 'babel-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/mobile',
+};
